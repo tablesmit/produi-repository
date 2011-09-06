65709d570444e7ed7001fc31ab6f28b0a753e6c2
hroarkDev <hroarkDev@gmail.com>
2011-09-06 13:05:15 +0000

    add 2 control unit tests

---
+++ ProdUITests/ProdUITests/ProdSpinnerTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Threading;
+using System.Windows.Automation;
+using NUnit.Framework;
+using ProdUI.AutomationPatterns;
+using ProdUI.Controls;
+using ProdUI.Session;
+using ProdUI.Exceptions;
+
+//WPF does not have equivelant
+namespace ProdUITests
+{
+    [TestFixture]
+    class ProdSpinnerTests
+    {
+        const string WIN_TITLE = "Winforms Test Form";
+        private static ProdSession session;
+        private static ProdWindow window;
+
+        [SetUp]
+        public static void Init()
+        {
+            session = new ProdSession("test.ses");
+            window = new ProdWindow(WIN_TITLE, session.Loggers);
+        }
+
+        [Test]
+        public void SetValue([Values(1.0, 12, 20)]double value)
+        {
+            ProdSpinner spin = new ProdSpinner(window, "testCheckBoxA");
+            spin.SetValue(value);
+            Assert.That(RangeValuePatternHelper.GetValue(spin.ThisElement), Is.EqualTo(value));
+        }
+
+        [Test]
+        public void SetValueEventNotification([Values(1.0, 12, 20)]double value)
+        {
+            ProdSpinner spin = new ProdSpinner(window, "testCheckBoxA");
+            spin.SetValue(value);
+
+            Thread.Sleep(2000);
+            Assert.That(spin.eventTriggered);
+        }
+
+        [Test]
+        public void GetValue([Values(1.0, 12, 20)]double value)
+        {
+            ProdSpinner spin = new ProdSpinner(window, "testCheckBoxA");
+            RangeValuePatternHelper.SetValue(spin.ThisElement, value);
+
+            Assert.That(spin.GetValue(), Is.EqualTo(value));
+        }
+
+        [Test]
+        public void GetMaxValue()
+        {
+            ProdSpinner spin = new ProdSpinner(window, "testCheckBoxA");
+            Assert.That(spin.GetMaxValue(), Is.GreaterThan(0));
+        }
+
+        [Test]
+        public void GetMinValue()
+        {
+            ProdSpinner spin = new ProdSpinner(window, "testCheckBoxA");
+            Assert.That(spin.GetMinValue(), Is.GreaterThan(0));
+        }
+
+    }
+
+}
---
+++ ProdUITests/ProdUITests/TabControlTests.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Automation;
+using NUnit.Framework;
+using ProdUI.AutomationPatterns;
+using ProdUI.Controls;
+using ProdUI.Session;
+using ProdUI.Utility;
+using System.Diagnostics;
+
+namespace ProdUITests
+{
+    [TestFixture]
+    class TabControlTests
+    {
+        const string WIN_TITLE = "WPF Test Form";
+        const int NUM_OF_TABS = 3;
+        private static ProdSession session;
+        private static ProdWindow window;
+
+        [SetUp]
+        public static void Init()
+        {
+            session = new ProdSession("test.ses");
+            window = new ProdWindow(WIN_TITLE, session.Loggers);
+        }
+
+        [Test]
+        public void GetItemCount()
+        {
+            ProdTab tabs = new ProdTab(window, "tabControl1");
+            Assert.That(tabs.GetItemCount(), Is.EqualTo(NUM_OF_TABS));
+            Debug.WriteLine(tabs.GetItemCount() + " = " + NUM_OF_TABS);
+        }
+
+        [Test]
+        public void GetItems()
+        {
+            ProdTab tabs = new ProdTab(window, "tabControl1");
+            List<object> items = tabs.GetItems(); //Note: not too sure what this will return
+        }
+
+        [Test]
+        public void IsSelected([Values(1, 2, 0)]int index)
+        {
+            ProdTab tabs = new ProdTab(window, "tabControl1");
+            tabs.SelectTab(index);
+
+            Assert.That(tabs.IsSelected(index));
+        }
+
+        [Test]
+        public void IsSelected([Values("one", "Two", "Three")]string itemText)
+        {
+            ProdTab tabs = new ProdTab(window, "tabControl1");
+            tabs.SelectTab(itemText);
+            Assert.That(tabs.IsSelected(itemText));
+        }
+
+        [Test]
+        public void TabCount()
+        {
+            ProdTab tabs = new ProdTab(window, "tabControl1");
+
+            Assert.That(tabs.TabCount(),Is.EqualTo(NUM_OF_TABS));
+        }
+
+        [Test]
+        public void SelectedTab([Values("two", "One", "Three")]string itemText)
+        {
+            ProdTab tabs = new ProdTab(window, "tabControl1");
+            tabs.SelectTab(itemText);
+            
+            /* the function to select the tab is not case-sensative, so the test isn't either */
+            int result = string.Compare(tabs.SelectedTab().Current.Name, itemText, true);
+            Assert.That(result == 0);
+        }
+
+        [Test]
+        public void SelectTab([Values(1, 2, 0)]int index)
+        {
+            ProdTab tabs = new ProdTab(window, "tabControl1");
+            tabs.SelectTab(index);
+
+            Assert.That(tabs.IsSelected(index));
+        }
+
+        [Test]
+        public void SelectTab([Values("one", "Two", "Three")]string itemText)
+        {
+            ProdTab tabs = new ProdTab(window, "tabControl1");
+            tabs.SelectTab(itemText);
+            AutomationElement tab = tabs.SelectedTab();
+
+            /* the function to select the tab is not case-sensative, so the test isn't either */
+            int result = string.Compare(tab.Current.Name,itemText,true);
+            Assert.That(result == 0);
+        }
+
+        [Test]
+        public void SelectTabEventNotification([Values(1)]int index)
+        {
+            ProdTab tabs = new ProdTab(window, "tabControl1");
+            tabs.SelectTab(index);
+
+            Thread.Sleep(2000);
+            Assert.That(tabs.eventTriggered);
+        }
+    }
+}
