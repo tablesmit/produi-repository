e47b44ea75fbcbbd4013122302a470993db1efe9
hroarkDev <hroarkDev@gmail.com>
2011-10-05 14:15:46 +0000

    Pre-release folder restructuring for easier deployment - step 2

--- Applications/MapLib/Properties/AssemblyInfo.cs
+++ Applications/MapLib/Properties/AssemblyInfo.cs
@@ -1,42 +1,44 @@
-﻿// License Rider: I really don't care how you use this code, or if you give credit. Just don't blame me for any damage you do
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Resources;
 
-// General Information about an assembly is controlled through the following
+//[assembly: SecurityPermission(SecurityAction.RequestMinimum, UnmanagedCode = false)]
+
+
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("DebugLogger")]
+[assembly: AssemblyTitle("MapLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ProdUI")]
-[assembly: AssemblyProduct("DebugLogger")]
+[assembly: AssemblyProduct("MapLib")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
-[assembly: Guid("ee35abbe-783d-4bc7-99f1-cb052876ac0c")]
+[assembly: Guid("0051c2e6-c55f-486e-9e5f-ea8593685bb2")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.1.2.0901")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.27.0434")]
+[assembly: AssemblyFileVersion("0.0.27.0434")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
