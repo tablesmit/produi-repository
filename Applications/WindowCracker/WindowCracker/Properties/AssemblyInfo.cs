959d28a9b737a5993992dd4ff2627b334ecc6ee7
hroarkDev <hroarkDev@gmail.com>
2011-10-07 20:27:45 +0000

    Moving some stuff around for project split

--- Applications/WindowCracker/WindowCracker/Properties/AssemblyInfo.cs
+++ Applications/WindowCracker/WindowCracker/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿// License Rider: I really don't care how you use this code, or if you give credit. Just don't blame me for any damage you do
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -38,3 +39,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
